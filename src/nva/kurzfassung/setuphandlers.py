09400e6547ded4985c902e43daf92e565e535fe5
lwalther <lwalther@novareto.de>
2019-05-05 21:50:35 +0200

    Installation der Ansichten

--- src/nva/kurzfassung/setuphandlers.py
+++ src/nva/kurzfassung/setuphandlers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
+from Products.CMFCore.utils import getToolByName
 from zope.interface import implementer
 
 
@@ -16,6 +17,48 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+
+    typesTool = getToolByName(context, 'portal_types')
+
+    typefolder = typesTool['Folder']
+    viewlist = typefolder.getProperty('view_methods', d=None)
+    if 'enhanced_foldersummary' not in viewlist:
+        viewlist = viewlist + ('enhanced_foldersummary',)
+    if 'enhanced_folderlist' not in viewlist:
+        viewlist = viewlist + ('enhanced_folderlist',)
+    if 'enhanced_cardview' not in viewlist:
+        viewlist = viewlist + ('enhanced_cardview',)
+    if 'nachricht_view' not in viewlist:
+        viewlist = viewlist + ('nachricht_view',)
+    if 'small_nachricht_view' not in viewlist:
+        viewlist = viewlist + ('small_nachricht_view',)
+    if 'nachricht_liste_view' not in viewlist:
+        viewlist = viewlist + ('nachricht_liste_view',)
+    if 'enhanced_contentdir' not in viewlist:
+        viewlist = viewlist + ('enhanced_contentdir',)
+    if 'enhanced_tableview' not in viewlist:
+        viewlist = viewlist + ('enhanced_tableview',)
+    typefolder.manage_changeProperties(view_methods = viewlist)
+
+    typecollection = typesTool['Collection']
+    viewlist = typecollection.getProperty('view_methods', d=None)
+    if 'enhanced_foldersummary' not in viewlist:
+        viewlist = viewlist + ('enhanced_foldersummary',)
+    if 'enhanced_folderlist' not in viewlist:
+        viewlist = viewlist + ('enhanced_folderlist',)
+    if 'enhanced_cardview' not in viewlist:
+        viewlist = viewlist + ('enhanced_cardview',)
+    if 'nachricht_view' not in viewlist:
+        viewlist = viewlist + ('nachricht_view',)
+    if 'small_nachricht_view' not in viewlist:
+        viewlist = viewlist + ('small_nachricht_view',)
+    if 'nachricht_liste_view' not in viewlist:
+        viewlist = viewlist + ('nachricht_liste_view',)
+    if 'enhanced_contentdir' not in viewlist:
+        viewlist = viewlist + ('enhanced_contentdir',)
+    if 'enhanced_tableview' not in viewlist:
+        viewlist = viewlist + ('enhanced_tableview',)
+    typecollection.manage_changeProperties(view_methods = viewlist)
 
 
 def uninstall(context):
