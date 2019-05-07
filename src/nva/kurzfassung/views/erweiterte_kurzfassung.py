e0e2bc509a46d62dc6dd95872e41e7e51ca9863f
Lars Walther <meinkraeks@gmail.com>
2019-05-07 18:34:42 +0200

    Umstellung der Ansichten auf Bootstrap 4.3

--- src/nva/kurzfassung/views/erweiterte_kurzfassung.py
+++ src/nva/kurzfassung/views/erweiterte_kurzfassung.py
@@ -118,7 +118,7 @@ class ErweiterteKurzfassung(BrowserView):
             contentid = selectSchmuckbilder(self.context).getTerm(obj.schmuckbild).token
             brain = api.content.find(portal_type='Image', UID=contentid)[0]
             imgobj = brain.getObject()
-            imgurl = '%s/@@images/image/mini' %imgobj.absolute_url()
+            imgurl = '%s/@@images/image/large' %imgobj.absolute_url()
             imgtitle = brain.Description
             image = imagetag %(imgurl, imgtitle)
         except:
@@ -131,7 +131,7 @@ class ErweiterteKurzfassung(BrowserView):
         Formatiert ein Vorschaubild fuer die Ordneransichten
         """
         image = ''
-        imagetag = '<img src="%s" title="%s" class="img-responsive">'
+        imagetag = '<img src="%s" title="%s" class="img-fluid">'
         imgtitle = obj.title
         if hasattr(obj, 'alttitle'):
             if obj.alttitle:
@@ -139,29 +139,29 @@ class ErweiterteKurzfassung(BrowserView):
         if hasattr(obj, 'titleimages'):
             if obj.titleimages:
                 if obj.titleimages[0].to_object:
-                    imgurl = '%s/@@images/image/mini' % obj.titleimages[0].to_object.absolute_url()
+                    imgurl = '%s/@@images/image/large' % obj.titleimages[0].to_object.absolute_url()
                     imgtitle = obj.titleimages[0].to_object.description
                     image = imagetag %(imgurl, imgtitle)
         if hasattr(obj, 'newsimage'):
             if obj.newsimage:
                 if obj.newsimage.to_object:
-                    imgurl = '%s/@@images/image/mini' % obj.newsimage.to_object.absolute_url()
+                    imgurl = '%s/@@images/image/large' % obj.newsimage.to_object.absolute_url()
                     imgtitle = obj.newsimage.to_object.title
                     image = imagetag %(imgurl, obj.newsimage.to_object.title)
         if hasattr(obj, 'poster'):
             if obj.poster:
-                imgurl = '%s/@@images/poster/mini' %obj.absolute_url()
+                imgurl = '%s/@@images/poster/large' %obj.absolute_url()
                 image = imagetag %(imgurl,imgtitle)
         if hasattr(obj, 'schmuckbild'):
             if obj.schmuckbild:
                 image = self.getSchmuckbild(imagetag, obj)
         if hasattr(obj, 'portraet'):
             if obj.portraet:
-                imgurl = '%s/@@images/portraet/mini' %obj.absolute_url()
+                imgurl = '%s/@@images/portraet/large' %obj.absolute_url()
                 image = imagetag %(imgurl, imgtitle)
         if hasattr(obj, 'image'):
             if obj.image:
-                imgurl = '%s/@@images/image/mini' %obj.absolute_url()
+                imgurl = '%s/@@images/image/large' %obj.absolute_url()
                 image = imagetag %(imgurl, imgtitle)
         if not image and obj.portal_type == "News Item":
             image = getDefault()
--- src/nva/kurzfassung/views/kleine_nachrichten_archiv_ansicht.py
+++ src/nva/kurzfassung/views/kleine_nachrichten_archiv_ansicht.py
@@ -8,5 +8,47 @@ from Products.Five.browser import BrowserView
 class KleineNachrichtenArchivAnsicht(NachrichtenArchivAnsicht):
     """ Erbt von der Nachrichten Ansicht """
 
+    def formatPreviewImage(self, obj):
+        """
+        Formatiert ein Vorschaubild fuer die Ordneransichten
+        """
+        image = ''
+        imagetag = '<img src="%s" title="%s" class="card-img">'
+        imgtitle = obj.title
+        if hasattr(obj, 'alttitle'):
+            if obj.alttitle:
+                imgtitle = obj.alttitle
+        if hasattr(obj, 'titleimages'):
+            if obj.titleimages:
+                if obj.titleimages[0].to_object:
+                    imgurl = '%s/@@images/image/large' % obj.titleimages[0].to_object.absolute_url()
+                    imgtitle = obj.titleimages[0].to_object.description
+                    image = imagetag %(imgurl, imgtitle)
+        if hasattr(obj, 'newsimage'):
+            if obj.newsimage:
+                if obj.newsimage.to_object:
+                    imgurl = '%s/@@images/image/large' % obj.newsimage.to_object.absolute_url()
+                    imgtitle = obj.newsimage.to_object.title
+                    image = imagetag %(imgurl, obj.newsimage.to_object.title)
+        if hasattr(obj, 'poster'):
+            if obj.poster:
+                imgurl = '%s/@@images/poster/large' %obj.absolute_url()
+                image = imagetag %(imgurl,imgtitle)
+        if hasattr(obj, 'schmuckbild'):
+            if obj.schmuckbild:
+                image = self.getSchmuckbild(imagetag, obj)
+        if hasattr(obj, 'portraet'):
+            if obj.portraet:
+                imgurl = '%s/@@images/portraet/large' %obj.absolute_url()
+                image = imagetag %(imgurl, imgtitle)
+        if hasattr(obj, 'image'):
+            if obj.image:
+                imgurl = '%s/@@images/image/large' %obj.absolute_url()
+                image = imagetag %(imgurl, imgtitle)
+        if not image and obj.portal_type == "News Item":
+            image = getDefault()
+
+        return image
+
     def aktuelllink(self):
         return self.context.absolute_url()
